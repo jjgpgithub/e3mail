0a61c7c4ef31878e471988e98b4ebdcebdf21845
79063 <79063@DESKTOP-A2TCFS0>
2018-12-31 17:40:16 +0800

    managet

---
+++ e3mall-manager/e3mall-manager-service/src/main/java/cn/e3/manager/service/impl/ItemServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.e3.manager.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3.manager.service.ItemService;
+import cn.e3.mapper.TbItemMapper;
+import cn.e3.pojo.TbItem;
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private TbItemMapper itemMapper;
+	@Override
+	public TbItem findItemByItemId(Long itemId) {
+		TbItem selectByPrimaryKey = itemMapper.selectByPrimaryKey(itemId);
+		return selectByPrimaryKey;
+	}
+
+}
